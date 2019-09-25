334fb59d218e577d7c82564496cdca11b6e1b4b3
Paimon <asuinform@mail.ru>
2019-09-25 08:50:42 +0300

    Добавлен отчет по первой лабе тау

--- ТАУ/labi/1/tau.m
+++ ТАУ/labi/1/tau.m
@@ -18,13 +18,28 @@ P = 1.4 * F_n * Vmax
 
 M_nm = 0.14 ;
 w_nm = 345;
-J_nm = 0.4*10^-4
+J_nm = 0.4*10^-4;
 
 i = w_nm / Vmax
 M_n = (J_nm * 0.1*J_nm + m/(0.8*i^2))* i * Wmax + (1/i*0.8) * F_n 
 
 
-U_pt = 27;
-I_ya = 2.7;
-R_ya = 1.7;
-N_ya = 0.51;
+
+
+M_nm = 0.15;
+w_nm = 360;
+J_nm = 0.7*10^-4;
+
+i = w_nm / Vmax
+M_n = (J_nm * 0.1*J_nm + m/(0.8*i^2))* i * Wmax + (1/i*0.8) * F_n
+
+
+
+
+
+M_nm = 0.21;
+w_nm = 314;
+J_nm = 0.5*10^-4;
+
+i = w_nm / Vmax
+M_n = (J_nm * 0.1*J_nm + m/(0.8*i^2))* i * Wmax + (1/i*0.8) * F_n 
